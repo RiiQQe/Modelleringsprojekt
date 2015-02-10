63303dc94b3fffa602354ac077e4087c597c3daf
novalain <micno751@student.liu.se>
2015-02-10 08:42:11 +0100

    FLUID med tok brute force

--- Modelleringsprojekt/Particle.cpp
+++ Modelleringsprojekt/Particle.cpp
@@ -10,7 +10,7 @@ using namespace glm;
 // Constructor for a particle.
 void Particle::CreateParticle()
 {
-	pos = vec2(0, 0 ) ;
+	pos = vec2(0, 0) ;
 	vel = vec2(0);
 
 	mass = 1.0f;
@@ -33,6 +33,26 @@ void Particle::CreateParticle()
 
 void Particle::EvolveParticle()
 {
+    //std::cout <<" VISC :" << gravity_force.y << std::endl;
+    
+    //s = v0 + at^2 /2
+    double newPosX = pos[0] + 0.1*vel[0] + 0.1*0.1*((gravity_force.x + pressure_force.x + viscousity_force.x) / density)/2;
+    double newPosY = pos[1] + 0.1*vel[1] + 0.1*0.1*((gravity_force.y + pressure_force.y + viscousity_force.y ) / density)/2;
+    
+    //v = s / t
+    double newVelX = (newPosX - pos[0])/0.1;
+    double newVelY = (newPosY - pos[1])/0.1;
+    
+   // vel[0] = vel[0] + 0.01*((gravity_force.x + pressure_force.x + viscousity_force.x) / density);
+    //vel[1] = vel[1] + 0.01*((gravity_force.y + pressure_force.y + viscousity_force.y ) / density);
+    
+    pos[0] = newPosX;
+    pos[1] = newPosY;
+    
+    vel[0] = newVelX;
+    vel[1] = newVelY;
+    
+    
     
     /*
 	vel[1] -= gravity * 0.01f;
@@ -48,29 +68,34 @@ void Particle::EvolveParticle()
 
 	pos[0] += vel[0];*/
     
-    if(pos.x <= 1){
+    if(pos.x < 1){
     	
+        vel.x = -vel.x;
         pos.x = 1;
-        vel.x = -0.7*vel.x;
+        
     }
     
-    else if(pos.x >= 512){
+    else if(pos.x > 512){
+        
+        vel.x = -vel.x;
         pos.x = 512;
-        vel.x = -0.7*vel.x;
+       
     	
     }
     
-    if(pos.y <= 1){
+    if(pos.y < 1){
     	
+        vel.y = -vel.y;
         pos.y = 1;
-        vel.y = -0.7*vel.y;
+        
     	
     }
     
-    else if(pos.y >= 512){
+    else if(pos.y > 512){
     	
+        vel.y = -vel.y;
         pos.y = 512;
-        vel.y = -0.7*vel.y;
+        
     
     }
     
@@ -125,10 +150,10 @@ void Particle::DrawObjects()
 
     !special ? glColor3f(0.2,0.2,1) :  glColor3f(1,1,1);
     glBegin(GL_TRIANGLE_STRIP);
-    glTexCoord2f(0.0,1.0); glVertex3f(pos[0]+1, pos[1]+1,0.5);     // top    right
-    glTexCoord2f(0.0,1.0); glVertex3f(pos[0]-1, pos[1]+1,0.5);     // top    left
-    glTexCoord2f(0.0,1.0); glVertex3f(pos[0]+1, pos[1]-1,0.5);     // bottom right
-    glTexCoord2f(0.0,1.0); glVertex3f(pos[0]-1, pos[1]-1,0.5);     // bottom left
+    glTexCoord2f(0.0,1.0); glVertex3f(pos[0]+2, pos[1]+2,0.5);     // top    right
+    glTexCoord2f(0.0,1.0); glVertex3f(pos[0]-2, pos[1]+2,0.5);     // top    left
+    glTexCoord2f(0.0,1.0); glVertex3f(pos[0]+2, pos[1]-2,0.5);     // bottom right
+    glTexCoord2f(0.0,1.0); glVertex3f(pos[0]-2, pos[1]-2,0.5);     // bottom left
     
     glEnd();
 	
--- Modelleringsprojekt/main.cpp
+++ Modelleringsprojekt/main.cpp
@@ -9,11 +9,11 @@
 #include <math.h>
 #include <vector>
 
-const int NUM_PARTICLES = 500;
-const float VISCOUSITY = 1.1f;
-const float PARTICLE_MASS = .02f;
-const double h = 1.5f;
-const float STIFFNESS = 1.5f;
+const int NUM_PARTICLES = 100;
+const float VISCOUSITY = 2.5f;
+const float PARTICLE_MASS = .14f;
+const double h = 0.032f;
+const float STIFFNESS = 3.f;
 
 Particle particles[NUM_PARTICLES];
 
@@ -22,20 +22,28 @@ Box box = Box();
 // Create all the particles
 void CreateParticles()
 {
-    
-    for(int i = 0; i < NUM_PARTICLES; i++)
-    {
+    for(int i = 0; i < NUM_PARTICLES; i++) {
         particles[i].CreateParticle();
-		
-        if (i == 0)
-			particles[i].setSpecial();
-    
-        glm::vec2 newPos = glm::vec2((float) 1.3 * (i % 40) + 100, (float) 0.27 * (i * 3 / 40) + 100);
-        particles[i].setPos(newPos);
-        
+
     }
     
+    int k = 0, j = 0;
+    
+    for(int i = 0; i < NUM_PARTICLES; i++){
+        
+        if(i % 10 == 0)
+            k++;
+        
+        if(i % 10 == 0)
+            j=0;
+        
+        j++;
+        
+        particles[i].setPos(glm::vec2(10 + j*0.2, 400 - k*0.2));
+        
+    }
 }
+
 
 void calculateDensity(){
     
@@ -73,19 +81,40 @@ void calculateDensity(){
                             
                             Particle neighbour = particles[j];
                             //std::cout << "NEIGHTBOUR : " << neighbour.getCell().x << std::endl;
-                            if(neighbour.getCell() == glm::vec2(x+offsetX, y+offsetY) && i != j){
+                            if(neighbour.getCell() == glm::vec2(x+offsetX, y+offsetY)){
                         
                                 glm::vec2 diffvec = p.getPos() - neighbour.getPos();
+                    
+                                diffvec/=512.f;
                                 
                                 //std::cout << "(" << neighbour.getPos().x << ", " << neighbour.getPos().y << ")" << std::endl;
                                 //std::cout << diffvec.x << std::endl;
                                 //std::cout << glm::length(diffvec) << std::endl;
                                 
+                                float abs_diffvec = glm::length(diffvec);
+                                
+                                //std::cout << "ABS DIFFVEC = " << abs_diffvec << std::endl;
+                                
+                                //Hoppas den aldrig går in här
+                                //if(abs_diffvec == 0)
+                                  //  std::cout << "ABS: : " << abs_diffvec << std::endl;
+                                
                                 //if smaller than h...
-                                if(glm::length(diffvec) < h){
+                                if(abs_diffvec < h){
+                                    
+                                   /* glBegin(GL_LINES);
+                                    glColor3f(1.f, 0.2f, 0.2f);
+                                    
+                                    glVertex2f(p.getPos().x, p.getPos().y);
+                                    glVertex2f(neighbour.getPos().x, neighbour.getPos().y);
+                                    
+                                    glEnd();*/
+
                                 	//density_sum += 1.505 * glm::pow(10, 10) * (glm::pow(h, 2.0) - glm::pow(glm::length(diffvec), 2.f));
                                     
                                     float diffvec_abs = glm::length(diffvec);
+                                    
+                                    //std::cout << "DIFFVEC AbS : " << diffvec_abs << std::endl;
                                     
                                     density_sum += PARTICLE_MASS * (315 / (64*M_PI * glm::pow(h, 9.0))) * glm::pow((glm::pow(h, 2.0) - glm::pow(diffvec_abs, 2.f)),3.0);
                                 
@@ -102,7 +131,8 @@ void calculateDensity(){
                 }
                 
 				//std::cout << "DENSITY SUM: " << density_sum << std::endl;
-                particles[i].setDensity(density_sum);
+                if(density_sum != 0)
+                	particles[i].setDensity(density_sum);
                 // End particle loop
                 
             }
@@ -124,94 +154,93 @@ void calculatePressure(){
     }
 
 }
-
 void calculatePressureAndViscousityGradients(){
-
-    // Col
-    for(int x = 0; x < 16; x++){
-        // Row
-        for(int y = 0; y < 16; y++){
-            
-            for(int i = 0; i < NUM_PARTICLES; i++){
-                
-                glm::vec2 pressure = glm::vec2(0);
-                glm::vec2 viscousity = glm::vec2(0);
-                glm::vec2 gravity = glm::vec2(0, -9.82f);
-                
-                //std::cout << "LOLOL" << particles[i].getPressure() << std::endl;
-                
-                Particle p = particles[i];
-
-                //Check only particles that is inside the current cell
-                if(p.getCell() != glm::vec2(x,y))
-                    continue;
-                
-                //std:: cout << "PARTICLE CELL = " << p.getCell().x << std::endl;;
-                
-                // Iterate through neighboring cells
-                for(int offsetX = -1; offsetX <= 1; offsetX++){
-                    
-                    //If out of bounds
-                    if (x+offsetX < 0) continue;
-                    if (x+offsetX >= 16) break;
-                    
-                    for(int offsetY = -1; offsetY <= 1; offsetY++){
-                        
-                        if (y+offsetY < 0) continue;
-                        if (y+offsetY >= 16) break;
-                        
-                        //Check all possible neighbours
-                        for(int j = 0; j < NUM_PARTICLES; j++){
-                            
-                            Particle neighbour = particles[j];
-                            //std::cout << "NEIGHTBOUR : " << neighbour.getCell().x << std::endl;
-                            if(neighbour.getCell() == glm::vec2(x+offsetX, y+offsetY) && i != j){
-                                
-                                glm::vec2 diffvec = p.getPos() - neighbour.getPos();
-                                
-                                float abs_diffvec = glm::length(diffvec);
-                                
-                                // std::cout << "diffvec x : " << abs_diffvec << std::endl;
-                                
-                                // if smaller than h...
-                                if(abs_diffvec < h){
-                                    
-                                    float W_const = 45/(M_PI * glm::pow(abs_diffvec, 6.f)) * glm::pow(h - abs_diffvec, 3.0) / abs_diffvec;
-                                    
-                                    glm::vec2 W_pressure_gradient = glm::vec2(W_const * diffvec.x, W_const * diffvec.y);
-                                    
-                                    float visc_gradient = W_const * (h - abs_diffvec);
-                                    
-                                    pressure +=  - PARTICLE_MASS * ((p.getPressure() + neighbour.getPressure()) / 2 * neighbour.getPressure()) * W_pressure_gradient;
-                                    
-                                    viscousity += VISCOUSITY * PARTICLE_MASS * (neighbour.getVelocity() - p.getVelocity()) * visc_gradient;
-                                    
-                                    //std::cout << "visc" << viscousity.x;
-                                    //pressure = -PARTICLE_MASS*((p.getPressure() + neighbour.getPressure())/2*neighbour.getPressure());
-                                   
-                                
-                                }
-                                
-                            }
-
-                        }
-                        
-                    }
-                    
-                }
-                
-                particles[i].setViscousityForce(viscousity);
-                particles[i].setPressureForce(pressure);
-                particles[i].setGravityForce(gravity);
-                // End particle loop
-            }
-            
-        }
-        
-    }
     
+    
+    
+     // Col
+     for(int x = 0; x < 16; x++){
+     // Row
+     	for(int y = 0; y < 16; y++){
+     
+     		for(int i = 0; i < NUM_PARTICLES; i++){
+     
+                glm::vec2 gravity = glm::vec2(0, -9.82f*particles[i].getDensity());
+     			glm::vec2 pressure = glm::vec2(0);
+     			glm::vec2 viscousity = glm::vec2(0);
+     
+     			//std::cout << "LOLOL" << particles[i].getPressure() << std::endl;
+     
+     			Particle p = particles[i];
+     
+     			//Check only particles that is inside the current cell
+     			if(p.getCell() != glm::vec2(x,y))
+     				continue;
+     
+     			//std:: cout << "PARTICLE CELL = " << p.getCell().x << std::endl;;
+     
+     			// Iterate through neighboring cells
+     			for(int offsetX = -1; offsetX <= 1; offsetX++){
+     
+     				//If out of bounds
+     				if (x+offsetX < 0) continue;
+                    if (x+offsetX >= 16) break;
+     
+     				for(int offsetY = -1; offsetY <= 1; offsetY++){
+     
+     					if (y+offsetY < 0) continue;
+     					if (y+offsetY >= 16) break;
+     
+     					//Check all possible neighbours
+     					for(int j = 0; j < NUM_PARTICLES; j++){
+     
+     						Particle neighbour = particles[j];
+     						//std::cout << "NEIGHTBOUR : " << neighbour.getCell().x << std::endl;
+     						if(neighbour.getCell() == glm::vec2(x+offsetX, y+offsetY) && i != j){
+     
+     							glm::vec2 diffvec = p.getPos() - neighbour.getPos();
+                                diffvec/=512;
+     
+     							float abs_diffvec = glm::length(diffvec);
+     							//abs_diffvec/=512;
+     
+     							// if smaller than h...
+     
+     							if(abs_diffvec < h){
+     
+     								float W_const_pressure = 45/(M_PI * glm::pow(h, 6.0)) * glm::pow(h - abs_diffvec, 3.0) / abs_diffvec;
+     
+     								glm::vec2 W_pressure_gradient = glm::vec2(W_const_pressure * diffvec.x, W_const_pressure * diffvec.y);
+     
+     								float visc_gradient = (45/(M_PI * glm::pow(h, 6.0)))*(h - abs_diffvec);
+     
+     								pressure +=  PARTICLE_MASS * ((p.getPressure() + neighbour.getPressure()) / (2 * neighbour.getDensity())) * W_pressure_gradient;
+     
+     								viscousity += VISCOUSITY * PARTICLE_MASS * ((neighbour.getVelocity() - p.getVelocity()) / (neighbour.getDensity())) * visc_gradient;
+     
+     								//std::cout << "visc" << viscousity.x;
+     								//pressure = -PARTICLE_MASS*((p.getPressure() + neighbour.getPressure())/2*neighbour.getPressure());
+     
+     
+     							}
+     
+     						}
+     
+     					}
+     
+     				}
+     
+     			}
+     
+     		particles[i].setViscousityForce(viscousity);
+            particles[i].setPressureForce(pressure);
+            particles[i].setGravityForce(gravity);
+            // End particle loop
+        	}
+     
+		}
+    }
 }
-
 
 void display()
 {
@@ -226,23 +255,23 @@ void display()
     //Update forces
     calculatePressureAndViscousityGradients();
     
-    glm::vec2 accelerationVec;
+    /*glm::vec2 accelerationVec;
     
     for(int i = 0; i < NUM_PARTICLES; i++){
     	
         accelerationVec = particles[i].getPressureForce() + particles[i].getViscousityForce() + particles[i].getGravityForce();
-        
+     
         //std::cout << "X VELOCITY : " << particles[i].getGravityForce().y << std::endl;
         //<< "Y VELOCITY : " << particles[i].getGravityForce().y << std::endl;
-        
+     
         particles[i].setVelocity(particles[i].getVelocity() + .01f*accelerationVec);
         particles[i].setPos(particles[i].getPos() + 1.f*particles[i].getVelocity());
-        
-    }
+     
+    }*/
     
     for(int i = 0; i <= NUM_PARTICLES; i++)
     {
-        //particles[i].EvolveParticle();
+        particles[i].EvolveParticle();
     }
 
     for(int i = 0; i <= NUM_PARTICLES; i++)
@@ -259,11 +288,11 @@ void reshape_window(GLFWwindow* window, int width, int height)
 
 void idle()
 {
-    
+    /*
     for (int i = 0; i < NUM_PARTICLES; i++)
     {
         particles[i].EvolveParticle();
-    }
+    }*/
     
     //glutPostRedisplay();
 }
