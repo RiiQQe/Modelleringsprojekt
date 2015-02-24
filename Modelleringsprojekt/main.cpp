eaea47d86a1a42f2240e5e7fd49b8909561ab754
Max Jonsson <max.jonsson@trustly.com>
2015-02-24 14:32:34 +0100

    added linear interpolation with meta balls and marching squares. Unoptimized as of now though.

--- Modelleringsprojekt/main.cpp
+++ Modelleringsprojekt/main.cpp
@@ -11,7 +11,7 @@
 
 #include <thread>
 
-const int NUM_PARTICLES = 5;
+const int NUM_PARTICLES = 9;
 const int GRID_WIDTH = 16;
 const int GRID_HEIGHT = 16;
 
@@ -19,7 +19,7 @@ Particle particles[NUM_PARTICLES];
 Box box = Box();
 
 Cell cells[GRID_WIDTH * GRID_HEIGHT];
-const int TEMPSIZE = 128;
+const int TEMPSIZE = 64;
 float squares[TEMPSIZE * TEMPSIZE]; // hard coded values for now, with marching squares
 int Particle::count = 1;
 
@@ -79,7 +79,7 @@ void display()
 	}
     
     for(int i = 0; i < NUM_PARTICLES; i++){
-        particles[i].DrawObjects();
+        //particles[i].DrawObjects();
     }
     
     // Beta balls begins
@@ -153,13 +153,6 @@ void display()
                 // These are used for interpolation process, increasing the possible angles from n * PI/4 to basically infinity.
                 float qx, qy, px, py;
                 
-                glTexCoord2f(0.0,1.0); glVertex3f((i + 1) * kSize + kSize / 2,     (j + 1) * kSize + kSize / 2,  0);     // top    right
-                glTexCoord2f(0.0,1.0); glVertex3f(i * kSize + kSize / 2,           (j + 1) * kSize + kSize / 2,  0);     // top    left
-                glTexCoord2f(0.0,1.0); glVertex3f((i + 1) * kSize + kSize / 2,     j * kSize + kSize / 2,        0);     // bottom right
-                glTexCoord2f(0.0,1.0); glVertex3f(i * kSize + kSize / 2,           j * kSize + kSize / 2,        0);     // bottom left
-                
-                
-                
                 // all points, position
                 float ax, ay, bx, by, cx, cy, dx, dy;
                 
@@ -179,117 +172,216 @@ void display()
                 dx = (i + 1) * kSize + kSize / 2;
                 dy =  j * kSize + kSize / 2;
                 
-                
-                
                 glBegin(GL_TRIANGLE_STRIP);
+                
                 glColor3f(1.f, 1.f, 1.f);
                 
-
                 switch (bitwiseSum) {
                     case 1:
+                        // Points of intersection of square bounadires from metaballs
+                        // left intersection
+                        qx = ax;
+                        qy = ay + (cy - ay) * ((1 - a) / (c - a));
+                        
+                        // upper intersection
+                        px = bx + (ax - bx) * ((1 - b) / (a - b));
+                        py = ay;
+                        
                         // top left triangle
                         glTexCoord2f(0.0,1.0); glVertex3f(ax,             ay,  0);  // top         left
-                        glTexCoord2f(0.0,1.0); glVertex3f(ax + kSize / 2, ay,  0);  // top         left+half
-                        glTexCoord2f(0.0,1.0); glVertex3f(cx, cy + kSize / 2,  0);  // bot+half    left
+                        glTexCoord2f(0.0,1.0); glVertex3f(px,             py,  0);  // top         left+half
+                        glTexCoord2f(0.0,1.0); glVertex3f(qx,             qy,  0);  // bot+half    left
                         break;
                     case 2:
+                        // Points of intersection of square boundaries from metaballs
+                        // right intersection
+                        qx = bx;
+                        qy = by + (dy - by) * ((1 - b) / (d - b));
+                        
+                        // upper intersection
+                        px = bx + (ax - bx) * ((1 - b) / (a - b));
+                        py = ay;
+                        
                         // top right triangle
                         glTexCoord2f(0.0,1.0); glVertex3f(bx,             by,  0);  // top          right
-                        glTexCoord2f(0.0,1.0); glVertex3f(ax + kSize / 2, ay,  0);  // top          left+half
-                        glTexCoord2f(0.0,1.0); glVertex3f(dx, dy + kSize / 2,  0);  // bot+half     right
+                        glTexCoord2f(0.0,1.0); glVertex3f(px,             py,  0);  // top          left+half
+                        glTexCoord2f(0.0,1.0); glVertex3f(qx,             qy,  0);  // bot+half     right
+                        
                         break;
                     case 3:
+                        // Points of intersection of square boundaries from metaballs
+                        // right intersection
+                        qx = bx;
+                        qy = by + (dy - by) * ((1 - b) / (d - b));
+                        
+                        // left intersection
+                        px = ax;
+                        py = ay + (cy - ay) * ((1 - a) / (c - a));
+                        
+                        
                         // top rectangle
                         glTexCoord2f(0.0,1.0); glVertex3f(bx,             by,  0);  // top      right
                         glTexCoord2f(0.0,1.0); glVertex3f(ax,             ay,  0);  // top      left
-                        glTexCoord2f(0.0,1.0); glVertex3f(dx, dy + kSize / 2,  0);  // bot+half right
-                        glTexCoord2f(0.0,1.0); glVertex3f(cx, cy + kSize / 2,  0);  // bot+half left
+                        glTexCoord2f(0.0,1.0); glVertex3f(qx,             qy,  0);  // bot+half right
+                        glTexCoord2f(0.0,1.0); glVertex3f(px,             py,  0);  // bot+half left
 
                         break;
                     case 4:
                         // Points of intersection of square boundaries from metaballs
+                        // right intersection
                         qx = bx;
                         qy = by + (dy - by) * ((1 - b) / (d - b));
                         
-                        px = dx + (cx - bx) * ((1 - d) / (c - d));
+                        // lower intersection
+                        px = dx + (cx - dx) * ((1 - d) / (c - d));
                         py = cy;
                         
                         // bot right triangle
-                        glTexCoord2f(0.0,1.0); glVertex3f((i + 1) * kSize + kSize / 2, j * kSize + kSize / 2,  0);   // bot right
-                        glTexCoord2f(0.0,1.0); glVertex3f(px, py, 0);
-                        glTexCoord2f(0.0,1.0); glVertex3f(qx, qy, 0);
+                        glTexCoord2f(0.0,1.0); glVertex3f(dx,             dy,  0);   // bot      right
+                        glTexCoord2f(0.0,1.0); glVertex3f(px,             py,  0);   // bot      left+half
+                        glTexCoord2f(0.0,1.0); glVertex3f(qx,             qy,  0);   // bot+half right
                         
                         break;
                     case 5:
                         std::cout << "SPECIAL CASE 5" << endl;
                         break;
                     case 6:
-                        // right rectangle
+                        // Points of intersection of square boundaries from metaballs
+                        // upper intersection
+                        qx = bx + (ax - bx) * ((1 - b) / (a - b));
+                        qy = ay;
                         
+                        // lower intersection
+                        px = dx + (cx - dx) * ((1 - d) / (c - d));
+                        py = cy;
+                        
+                        // right rectangle
                         glTexCoord2f(0.0,1.0); glVertex3f(bx,             by,  0);    // top    right
-                        glTexCoord2f(0.0,1.0); glVertex3f(ax + kSize / 2, ay,  0);    // top    left+half
+                        glTexCoord2f(0.0,1.0); glVertex3f(qx,             qy,  0);    // top    left+half
                         glTexCoord2f(0.0,1.0); glVertex3f(dx,             dy,  0);    // bottom right
-                        glTexCoord2f(0.0,1.0); glVertex3f(cx + kSize / 2, cy,  0);    // bottom left+half
+                        glTexCoord2f(0.0,1.0); glVertex3f(px,             py,  0);    // bottom left+half
                         
                         break;
                     case 7:
-                        // missing bot left triangle
+                        // Points of intersection of square boundaries from metaballs
+                        // left intersection
+                        qx = ax;
+                        qy = ay + (cy - ay) * ((1 - a) / (c - a));
                         
+                        // lower intersection
+                        px = dx + (cx - dx) * ((1 - d) / (c - d));
+                        py = cy;
+                        
+                        // missing bot left triangle
                         glTexCoord2f(0.0,1.0); glVertex3f(ax,             ay,  0);    // top        left
                         glTexCoord2f(0.0,1.0); glVertex3f(bx,             by,  0);    // top        right
-                        glTexCoord2f(0.0,1.0); glVertex3f(cx, cy + kSize / 2,  0);    // bot+half   left
-                        glTexCoord2f(0.0,1.0); glVertex3f(cx + kSize / 2, cy,  0);    // bot        left+half
+                        glTexCoord2f(0.0,1.0); glVertex3f(qx,             qy,  0);    // bot+half   left
+                        glTexCoord2f(0.0,1.0); glVertex3f(px,             py,  0);    // bot        left+half
                         glTexCoord2f(0.0,1.0); glVertex3f(bx,             by,  0);    // top        right
                         glTexCoord2f(0.0,1.0); glVertex3f(dx,             dy,  0);    // bot        right
                         break;
                     case 8:
+                        // Points of intersection of square boundaries from metaballs
+                        // left intersection
+                        qx = ax;
+                        qy = ay + (cy - ay) * ((1 - a) / (c - a));
+                        
+                        // lower intersection
+                        px = dx + (cx - dx) * ((1 - d) / (c - d));
+                        py = cy;
+                        
                         // bot left triangle
                         glTexCoord2f(0.0,1.0); glVertex3f(cx,             cy,   0);   // bottom      left
-                        glTexCoord2f(0.0,1.0); glVertex3f(cx, cy + kSize / 2,   0);   // bottom+half left
-                        glTexCoord2f(0.0,1.0); glVertex3f(cx + kSize / 2, cy,   0);   // bottom      left+half
+                        glTexCoord2f(0.0,1.0); glVertex3f(qx,             qy,   0);   // bottom+half left
+                        glTexCoord2f(0.0,1.0); glVertex3f(px,             py,   0);   // bottom      left+half
                         break;
                     case 9:
+                        // Points of intersection of square boundaries from metaballs
+                        // upper intersection
+                        qx = bx + (ax - bx) * ((1 - b) / (a - b));
+                        qy = ay;
+                        
+                        // lower intersection
+                        px = dx + (cx - dx) * ((1 - d) / (c - d));
+                        py = cy;
+                        
                         // left rectangle
                         glTexCoord2f(0.0,1.0); glVertex3f(ax,             ay,   0);   // top    left
                         glTexCoord2f(0.0,1.0); glVertex3f(cx,             cy,   0);   // bottom left
-                        glTexCoord2f(0.0,1.0); glVertex3f(ax + kSize / 2, ay,   0);   // top    left+half
-                        glTexCoord2f(0.0,1.0); glVertex3f(cx + kSize / 2, cy,   0);   // bottom left+half
+                        glTexCoord2f(0.0,1.0); glVertex3f(qx,             qy,   0);   // top    left+half
+                        glTexCoord2f(0.0,1.0); glVertex3f(px,             py,   0);   // bottom left+half
                         
                         break;
                     case 10:
                         std::cout << "SPECIAL CASE 10" << endl;
                         break;
                     case 11:
+                        // Points of intersection of square boundaries from metaballs
+                        // right intersection
+                        qx = bx;
+                        qy = by + (dy - by) * ((1 - b) / (d - b));
+                        
+                        // lower intersection
+                        px = dx + (cx - dx) * ((1 - d) / (c - d));
+                        py = cy;
+                        
                         // missing bot right triangle
                         glTexCoord2f(0.0,1.0); glVertex3f(ax,             ay,  0);    // top    left
                         glTexCoord2f(0.0,1.0); glVertex3f(bx,             by,  0);    // top    right
                         glTexCoord2f(0.0,1.0); glVertex3f(cx,             cy,  0);    // bottom left
-                        glTexCoord2f(0.0,1.0); glVertex3f(dx, dy + kSize / 2,  0);    // bot+half right
-                        glTexCoord2f(0.0,1.0); glVertex3f(cx + kSize / 2, cy,  0);    // bottom left+half
-                        
+                        glTexCoord2f(0.0,1.0); glVertex3f(qx,             qy,  0);    // bot+half right
+                        glTexCoord2f(0.0,1.0); glVertex3f(px,             py,  0);    // bottom left+half
                         break;
                     case 12:
+                        // Points of intersection of square boundaries from metaballs
+                        qx = bx;
+                        qy = by + (dy - by) * ((1 - b) / (d - b));
+                        
+                        // left intersection
+                        px = ax;
+                        py = ay + (cy - ay) * ((1 - a) / (c - a));
+                        
+                        
                         // bot rectangle
                         glTexCoord2f(0.0,1.0); glVertex3f(dx,             dy,  0);    // bottom right
                         glTexCoord2f(0.0,1.0); glVertex3f(cx,             cy,  0);    // bottom left
-                        glTexCoord2f(0.0,1.0); glVertex3f(dx, dy + kSize / 2,  0);    // bot+half right
-                        glTexCoord2f(0.0,1.0); glVertex3f(cx, cy + kSize / 2,  0);    // bot+half left
+                        glTexCoord2f(0.0,1.0); glVertex3f(qx,             qy,  0);    // bot+half right
+                        glTexCoord2f(0.0,1.0); glVertex3f(px,             py,  0);    // bot+half left
 
                         break;
                     case 13:
+                        // Points of intersection of square boundaries from metaballs
+                        // right intersection
+                        qx = bx;
+                        qy = by + (dy - by) * ((1 - b) / (d - b));
+                        
+                        // upper intersection
+                        px = bx + (ax - bx) * ((1 - b) / (a - b));
+                        py = ay;
+                        
                         // missing top right triangle
                         glTexCoord2f(0.0,1.0); glVertex3f(ax,             ay,  0);    // top    left
-                        glTexCoord2f(0.0,1.0); glVertex3f(ax + kSize / 2, ay,  0);    // top    left+half
+                        glTexCoord2f(0.0,1.0); glVertex3f(px,             py,  0);    // top    left+half
                         glTexCoord2f(0.0,1.0); glVertex3f(cx,             cy,  0);    // bottom left
-                        glTexCoord2f(0.0,1.0); glVertex3f(dx, dy + kSize / 2,  0);    // bot+half right
+                        glTexCoord2f(0.0,1.0); glVertex3f(qx,             qy,  0);    // bot+half right
                         glTexCoord2f(0.0,1.0); glVertex3f(dx,             dy,  0);    // bottom right
-                        
                         
                         break;
                     case 14:
+                        
+                        
+                        // Points of intersection of square bounadires from metaballs
+                        // left intersection
+                        qx = ax;
+                        qy = ay + (cy - ay) * ((1 - a) / (c - a));
+                        
+                        // upper intersection
+                        px = bx + (ax - bx) * ((1 - b) / (a - b));
+                        py = ay;
+                        
                         // missing top left triangle
-                        glTexCoord2f(0.0,1.0); glVertex3f(cx, cy + kSize / 2,  0);    // bot+half left
-                        glTexCoord2f(0.0,1.0); glVertex3f(ax + kSize / 2, ay,  0);    // top    left+half
+                        glTexCoord2f(0.0,1.0); glVertex3f(qx,             qy,  0);    // bot+half left
+                        glTexCoord2f(0.0,1.0); glVertex3f(px,             py,  0);    // top    left+half
                         glTexCoord2f(0.0,1.0); glVertex3f(cx,             cy,  0);    // bottom left
                         glTexCoord2f(0.0,1.0); glVertex3f(bx,             by,  0);    // top    right
                         glTexCoord2f(0.0,1.0); glVertex3f(dx,             dy,  0);    // bottom right
@@ -304,6 +396,7 @@ void display()
                         
                         break;
                     default:
+                        cout << "THIS IS FUCKED" << endl;
                         break;
                 }
                 
